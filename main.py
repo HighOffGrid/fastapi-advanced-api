c17d6d4b07b7efbbeb9d7bfe0184e76b2919770c
Iago Sena <iagosena.c@hotmail.com>
2026-03-10 18:55:10 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import Base, engine, get_db
+from app import crud, schemas
+from app.routers import products
+import app.models
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="FastAPI + PostgreSQL Example")
+
+app.include_router(products.router)
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_users(db, skip=skip, limit=limit)
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
