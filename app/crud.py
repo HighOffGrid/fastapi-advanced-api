c8aa7c5dd1c7d1d6e7a801072cd01eb349ab77ed
Iago <iagosena.c@hotmail.com>
2026-03-16 05:24:57 -0300

    Initial commit FastAPI avançado

---
+++ app/crud.py
@@ -0,0 +1,19 @@
+from app.models import User
+from app.schemas import UserCreate
+from sqlalchemy.orm import Session
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
---
+++ app/routers/products.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app import crud, schemas
+
+router = APIRouter(
+    prefix="/products",
+    tags=["products"]
+    )
+
+@router.get("/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_users(db, skip=skip, limit=limit)
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db, user)
